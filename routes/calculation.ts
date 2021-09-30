9e618cbb60376f74a7ab24b5d0855d7e0f3adbbd
工藤 正隆 <kudoma@istyle.local>
2021-09-30 19:09:53 +0900

    add TODO

--- routes/calculation.ts
+++ routes/calculation.ts
@@ -2,6 +2,7 @@ import { Request, Response, Router } from 'express';
 
 const router: Router = Router();
 
+// FIX: 冗長なコードを関数に吐きだす
 router
   .get('/', (req: Request, res: Response) => {
     res.json('Hello calculation api!!!!!');
@@ -9,25 +10,25 @@ router
   .get('/add', (req: Request, res: Response) => {
     let item1: number = Number(req.query.item1);
     let item2: number = Number(req.query.item2);
-    let result = item1 + item2;
+    let result: number = item1 + item2;
     res.json({ 'result': result });
   })
   .get('/subtract', (req: Request, res: Response) => {
     let item1: number = Number(req.query.item1);
     let item2: number = Number(req.query.item2);
-    let result = item1 - item2;
+    let result: number = item1 - item2;
     res.json({ 'result': result });
   })
   .get('/multiply', (req: Request, res: Response) => {
     let item1: number = Number(req.query.item1);
     let item2: number = Number(req.query.item2);
-    let result = item1 * item2;
+    let result: number = item1 * item2;
     res.json({ 'result': result });
   })
   .get('/divide', (req: Request, res: Response) => {
     let item1: number = Number(req.query.item1);
     let item2: number = Number(req.query.item2);
-    let result = item1 / item2;
+    let result: number = item1 / item2;
     res.json({ 'result': result });
   });
 
